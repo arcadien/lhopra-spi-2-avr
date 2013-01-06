aa11eb8cf6994af516efca54377279b989c03a58
Aurélien Labrosse <aurelien.labrosse@gmail.com>
2013-01-06 14:20:21 +0100

    reduced code size, ready for comments

--- Lhopra-spi-2-avr/main.cpp
+++ Lhopra-spi-2-avr/main.cpp
@@ -3,82 +3,17 @@
 
 const int ledPin = 13;
 
-/* Write an int as a {0,1} string
- *
- * buffer must have length >= sizeof(int) + 1
- * Write to the buffer backwards so that the binary representation
- * is in the correct order i.e.  the LSB is on the far right
- * instead of the far left of the printed string
- *
- */
-#define BUF_SIZE 9
-char *int2bin(int a, char *buffer, int buf_size) {
-	buffer += (buf_size - 1);
-
-	for (int i = 7; i >= 0; i--) {
-		*buffer-- = (a & 1) + '0';
-
-		a >>= 1;
-	}
-
-	return buffer;
-}
-
-/**
- * Convenient method used while configuring system clock
- */
-void dumpInfo() {
-
-	Serial.print("--- Mega2560 @ ");
-	Serial.print(F_CPU / 1000000);
-	Serial.println(" Mhz --- ");
-
-	// clock management register
-	char buffer[BUF_SIZE];
-
-	buffer[BUF_SIZE - 1] = '\0';
-	Serial.print("CLKPR (CLOCK MNGMT)");
-	Serial.print(" : [");
-	int2bin(CLKPR, buffer, BUF_SIZE - 1);
-	Serial.print(buffer);
-	Serial.println("]");
-
-	buffer[BUF_SIZE - 1] = '\0';
-	Serial.print("SPCR (SPI)");
-	Serial.print(" : [");
-	int2bin(SPCR, buffer, BUF_SIZE - 1);
-	Serial.print(buffer);
-	Serial.println("]");
-
-	buffer[BUF_SIZE - 1] = '\0';
-	Serial.print("SPSR (SPI)");
-	Serial.print(" : [");
-	int2bin(SPSR, buffer, BUF_SIZE - 1);
-	Serial.print(buffer);
-	Serial.println("]");
-
-	buffer[BUF_SIZE - 1] = '\0';
-	Serial.print("SPDR (SPI)");
-	Serial.print(" : [");
-	int2bin(SPDR, buffer, BUF_SIZE - 1);
-	Serial.print(buffer);
-	Serial.println("]");
-
-}
-
 void setup() {
 
 	pinMode(ledPin, OUTPUT);
 
-	// re-set the default configuration
-	SPI.setDataMode(SPI_MODE0); //          SPCR = (SPCR & ~SPI_MODE_MASK) | SPI_MODE0;
+	SPI.setDataMode(SPI_MODE0);
 
-	SPI.setBitOrder(MSBFIRST); //           SPCR &= ~(_BV(DORD));
+	SPI.setBitOrder(MSBFIRST);
 
-	SPI.setClockDivider(SPI_CLOCK_DIV8); // SPCR = (SPCR & ~SPI_CLOCK_MASK) | (SPI_CLOCK_DIV8 & SPI_CLOCK_MASK);
-										 // SPSR = (SPSR & ~SPI_2XCLOCK_MASK) | ((SPI_CLOCK_DIV8 >> 2) & SPI_2XCLOCK_MASK);
+	SPI.setClockDivider(SPI_CLOCK_DIV8);
 
-	SPI.begin(); // other pins configuration (OUTPUTs, default Slave Select)
+	SPI.begin();
 
 	Serial.begin(115200);
 
@@ -88,38 +23,26 @@ void setup() {
 	}
 
 	delay(2000);
-	dumpInfo();
-}
-
-void serialEvent() {
-
 }
 
 int main(void) {
-	init();
 
 	setup();
 
 	for (;;) {
 		while (Serial.available()) {
 
-			// get the new byte
 			char inChar = (char) Serial.read();
 
-			char buffer[BUF_SIZE];
-
-			buffer[BUF_SIZE - 1] = '\0';
-			int2bin(inChar, buffer, BUF_SIZE - 1);
-			Serial.print("read : ");
-			Serial.println(buffer);
-
+			digitalWrite(ledPin, HIGH);
 			// transfer
 			digitalWrite(SS, LOW);
 			SPI.transfer(inChar);
 			digitalWrite(SS, HIGH);
 
+			digitalWrite(ledPin, LOW);
+
 		}
 	}
-
 	return 0;
 }
