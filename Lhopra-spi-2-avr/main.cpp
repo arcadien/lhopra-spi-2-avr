8ed11cdc75b47af31c0debdfbf0846c3ed800537
Aurélien Labrosse <aurelien.labrosse@gmail.com>
2013-01-06 14:25:00 +0100

    commenté

--- Lhopra-spi-2-avr/main.cpp
+++ Lhopra-spi-2-avr/main.cpp
@@ -1,20 +1,31 @@
+/**
+ *
+ * Simple application qui attend des saisies sur un UART et
+ * transfère ces saisies dans un canal SPI. Les pins standards
+ * étant utilisés pour les branchements matériels, il y à très
+ * peu a faire ici.
+ *
+ */
+
 #include "arduino/Arduino.h"
 #include "arduino/libraries/SPI/SPI.h"
 
-const int ledPin = 13;
-
 void setup() {
 
-	pinMode(ledPin, OUTPUT);
-
+	// pour les phase & polarité de l'horloge
 	SPI.setDataMode(SPI_MODE0);
 
+	// par défaut, mais re-configuré
 	SPI.setBitOrder(MSBFIRST);
 
+	// SPI_CLOCK_DIV8 nous donne une frequence de 2Mhz
+	// puisque la carte tourne a 16Mhz
 	SPI.setClockDivider(SPI_CLOCK_DIV8);
 
+	// activation des pins
 	SPI.begin();
 
+	// activation du lien UART via la connexion USB
 	Serial.begin(115200);
 
 	// purge UART
@@ -30,17 +41,17 @@ int main(void) {
 	setup();
 
 	for (;;) {
+
+		// transfère dès qu'un caractère est lisible sur l'UART
+
 		while (Serial.available()) {
 
 			char inChar = (char) Serial.read();
 
-			digitalWrite(ledPin, HIGH);
 			// transfer
 			digitalWrite(SS, LOW);
 			SPI.transfer(inChar);
 			digitalWrite(SS, HIGH);
-
-			digitalWrite(ledPin, LOW);
 
 		}
 	}
